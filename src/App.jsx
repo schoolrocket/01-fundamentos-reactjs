c5bdafefce3f0f68896a13030aeca78ecaf7c6a0
namevug <ciavug@gmail.com>
2023-03-21 21:42:27 -0300

    feat: First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post"
+import './styles.css'
+
+export function App() {
+  return (
+    <>
+      <Post 
+        author="Name Vug" 
+        content="Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
+        />
+      
+      <Post 
+      author="Danilo" 
+      content="Mollitia magnam accusamus culpa sunt aliquid ratione ipsa. Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
+      />
+    </>
+  )
+}

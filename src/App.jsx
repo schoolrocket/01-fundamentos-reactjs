e043d68a980667b52fabc16f854ce19ae7315e49
namevug <ciavug@gmail.com>
2023-03-22 22:11:53 -0300

    feat: Import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
-import './styles.css'
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
+    <Header />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+
+      <main>
       <Post 
         author="Name Vug" 
         content="Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
@@ -13,6 +22,8 @@ export function App() {
       author="Danilo" 
       content="Mollitia magnam accusamus culpa sunt aliquid ratione ipsa. Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
       />
+      </main>
+    </div>
     </>
   )
 }

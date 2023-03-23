3f6343e75e820f652ba24972a207517574b1797d
namevug <ciavug@gmail.com>
2023-03-22 22:10:31 -0300

    feat: Created component Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+
+import React from 'react'
+
+export function Sidebar() {
+  return (
+    <>
+      <aside className={styles.sidebar}>
+        <img 
+          className={styles.cover}
+          src='https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
+        />
+        <div className={styles.profile}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/97545136?v=4" />
+
+          <strong>Victor Machado</strong>
+          <span>Web Developer</span>
+        </div>
+        <footer>
+          <a href='#'>Editar seu perfil</a>
+        </footer>
+      </aside>
+    </>
+  )
+}

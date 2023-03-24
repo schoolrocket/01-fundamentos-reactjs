a33a89fc455dacc21a9080dbb5c694c1b32fe59c
namevug <ciavug@gmail.com>
2023-03-24 14:48:02 -0300

    feat: Create component post

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 import styles from './App.module.css'
 import './global.css'
-import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
     <Header />
-
     <div className={styles.wrapper}>
       <Sidebar />
 
       <main>
-      <Post 
-        author="Name Vug" 
-        content="Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
-        />
-      
-      <Post 
-      author="Danilo" 
-      content="Mollitia magnam accusamus culpa sunt aliquid ratione ipsa. Repudiandae asperiores, tenetur ratione provident, optio molestiae quisquam alias ut illo eveniet dicta maiores." 
-      />
+        <Post />
+        <Post />
       </main>
     </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return ( 
+    <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/97545136?v=4" />
+                <div className={styles.authorInfo}>
+                    <strong>Victor Machado</strong>
+                    <span>Web Developer</span>
+                </div>
+            </div>
+            <time title="24 de Março às 11:00h" dateTime="2023-03-24 11:13:00">Publicado há 1h</time>
+        </header>
+        <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+            <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+            <p>
+                <a href="">#novoprojeto</a>{' '}
+                <a href="">#nlw</a>{' '}
+                <a href="">#rocketseat</a>
+            </p>
+        </div>
+    </article>
+    )
+}

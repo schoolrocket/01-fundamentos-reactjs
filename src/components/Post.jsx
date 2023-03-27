15741f013dbbc3bf2c371fb8b5ce6a06c688a4ed
namevug <ciavug@gmail.com>
2023-03-27 20:58:47 -0300

    feat: Comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -22,6 +23,19 @@ export function Post() {
                 <a href="">#nlw</a>{' '}
                 <a href="">#rocketseat</a>
             </p>
+        </div>
+        <form className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>
+            <textarea placeholder="Escreva um comentário..." />
+            <footer>
+                <button type="submit">Publicar</button>
+            </footer>
+        </form>
+
+        <div className={styles.commentList}>
+            <Comment />
+            <Comment />
+            <Comment />
         </div>
     </article>
     )

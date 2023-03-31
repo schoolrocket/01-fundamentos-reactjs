a71f185ca0624771462d3fabec74a3ac71bf8a1f
namevug <ciavug@gmail.com>
2023-03-30 23:17:12 -0300

    Adjust Post and Date Dinamic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import { Post } from "./components/Post"
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/97545136?v=4',
+      name: 'Victor Machado',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date ('2023-03-24 11:13:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/jovemdan',
+      name: 'Danilo Menezes',
+      role: 'Software Engineer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date ('2023-03-24 11:13:00')
+  },
+];
+
 export function App() {
   return (
     <>
@@ -12,9 +43,15 @@ export function App() {
       <Sidebar />
 
       <main>
-        <Post />
-        <Post />
-        <Post />
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
       </main>
     </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,41 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return ( 
     <article className={styles.post}>
         <header>
             <div className={styles.author}>
-                <Avatar src="https://avatars.githubusercontent.com/u/97545136?v=4" />
+                <Avatar src={author.avatarUrl} />
                 <div className={styles.authorInfo}>
-                    <strong>Victor Machado</strong>
-                    <span>Web Developer</span>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
                 </div>
             </div>
-            <time title="24 de Março às 11:00h" dateTime="2023-03-24 11:13:00">Publicado há 1h</time>
+            <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeToNow}
+            </time>
         </header>
         <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-            <p>
-                <a href="">#novoprojeto</a>{' '}
-                <a href="">#nlw</a>{' '}
-                <a href="">#rocketseat</a>
-            </p>
+            {content.map(line => {
+                if (line.type === 'paragraph') {
+                    return <p>{line.content}</p>;
+                } else if (line.type === 'link') {
+                    return <p><a href="#">{line.content}</a></p>;
+                }
+            })}
         </div>
         <form className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>

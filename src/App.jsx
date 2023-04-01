fc32205a52f4f1f16f232fcd73498fc889e72719
namevug <ciavug@gmail.com>
2023-03-31 22:48:59 -0300

    feat: Changing date

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date ('2023-03-24 11:13:00')
+    publishedAt: new Date ('2023-03-30 23:00:00')
   },
   {
     id: 2,

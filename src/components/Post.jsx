a7d64f649bede366b6fad87ee2ba8a5c0b27674e
namevug <ciavug@gmail.com>
2023-04-04 15:43:51 -0300

    feat: Resolve error Key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -48,9 +48,9 @@ function handleNewCommentChange() {
         <div className={styles.content}>
             {content.map(line => {
                 if (line.type === 'paragraph') {
-                    return <p>{line.content}</p>;
+                    return <p key={line.content}>{line.content}</p>;
                 } else if (line.type === 'link') {
-                    return <p><a href="#">{line.content}</a></p>;
+                    return <p key={line.content}><a href="#">{line.content}</a></p>;
                 }
             })}
         </div>
@@ -69,7 +69,7 @@ function handleNewCommentChange() {
 
         <div className={styles.commentList}>
             {comments.map(comment => {
-                return <Comment content={comment} />
+                return <Comment key={comment} content={comment} />
             })}
         </div>
     </article>

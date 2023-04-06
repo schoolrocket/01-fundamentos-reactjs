f8ed531faca599ae961226cb228a98158752d893
namevug <ciavug@gmail.com>
2023-04-06 16:24:59 -0300

    feat: Like the comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { Avatar } from './Avatar'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+  
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -26,9 +33,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>        
+            Aplaudir <span>{likeCount}</span>        
           </button>
         </footer>
       </div>
